b118dfcb30a1aac20105d9661537c89af26fb9eb
aaryamannchallani <aaryamannchallani7@gmail.com>
2020-06-24 23:08:17 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+    return (
+        <div className="container justify-content-center text-center">
+           <h3 className="display-6">
+               App
+           </h3>
+            
+        </div>
+    )
+}
+
+export default App
